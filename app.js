acca31f68bdde7ff397b1b39b2563aa35210d5ad
Fionn Tolster <fionntolster@invisionapp.com>
2019-10-23 16:58:39 +0800

    Databse module completed

--- app.js
+++ app.js
@@ -1,31 +1,22 @@
 // Modules to control application life and create native browser window
 const electron = require('electron')
-const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 const path = require('path')
+
+const app = electron.app
 const db = require('./static/modules/db.js')
-db.initialise
+const defaultPath = path.join(__dirname + '/static/defaults.json')
+const storePath = path.join(app.getPath('userData') + '/preferences.json')
+db.initialise({'default': defaultPath, 'storage': storePath, 'reset': false})
 
 // console.log(screen.get({name: 'height'}))
-db.set({
-  'name': 'dimensions',
-  'value': {
-    'axis': {
-      'etc': {
-        'z': '8'
-      }
-    }
-  }
-}).then((data) => {
-  console.log(data)
-})
-
+db.set('screen', {'width': 1650, 'height': 1500, 'frame': false})
 const locals = {
   /* ...*/
 }
 const setupPug = require('electron-pug')
 // Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+// be closed automatically when the JavaScript object is garbage conpmllected.
 let mainWindow
 
 const launch = async () => {
@@ -40,38 +31,43 @@ const launch = async () => {
   // console.log(width, height)
   // Create the browser window.
   // const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize
-  mainWindow = new BrowserWindow({
-    width: '100px',
-    height: '100px',
-    skipTaskbar: true,
-    title: 'Stager',
-    frame: true,
-    acceptFirstMouse: true,
-    hasShadow: true,
-    titleBarStyle: 'hidden',
-    center: true,
-    resizable: true,
-    simpleFullscreen: true,
-    webPreferences: {
-      devTools: true,
-      scrollBounce: true,
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+  db.get(
+    'screen',
+    (screen) => {
+      mainWindow = new BrowserWindow({
+        width: screen.width,
+        height: screen.height,
+        skipTaskbar: screen.skipTaskbar,
+        title: screen.title,
+        acceptFirstMouse: screen.acceptFirstMouse,
+        hasShadow: screen.hasShadow,
+        titleBarStyle: screen.titleBarStyle,
+        center: screen.center,
+        frame: screen.frame,
+        resizable: screen.resizable,
+        simpleFullscreen: screen.simpleFullscreen,
+        webPreferences: {
+          devTools: true,
+          scrollBounce: true,
+          preload: path.join(__dirname, 'preload.js')
+        }
+      })
+      mainWindow.loadFile('static/pages/index.pug')
+      // Open the DevTools.
+      // mainWindow.webContents.openDevTools()
+
+      // Emitted when the window is closed.
+      mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+      })
+    },
+    true
+  )
 
   // and load the index.html of the app.
-  mainWindow.loadFile('static/app.pug')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
 }
 
 // This method will be called when Electron has finished
--- static/modules/db.js
+++ static/modules/db.js
@@ -4,118 +4,215 @@ const fs = require('fs')
 const _object = require('lodash/fp/object')
 // Renderer process has to get `input` module via `remote`, whereas the main process can get it directly
 // input.getPath('userData') will return a string of the user's input data directory path.
-const userPref = path.join(
-  (electron.app || electron.remote.app).getPath('userData') +
-    '/preferences.json'
-)
-const defaults = path.join(__dirname, '../defaults.json')
+const options = {
+  storage: undefined,
+  default: undefined
+}
 let database
 
-let initialise = new Promise((resolve, reject) => {
-  if (database) resolve(database)
-  else {
-    try {
-      fs.exists(userPref, (exists) => {
-        if (exists) {
-          fs.readFile(userPref, (err, data) => {
-            if (err) console.log('here ' + err)
-            database = JSON.parse(data)
-            resolve(database)
-          })
-        } else {
-          fs.exists(defaults, (exists) => {
+// handles buffering file and enables referencing, refreshing
+const initialise = async (opts) => {
+  // removes the need to specify defaults on every call
+  // by persisting any passed settings
+  if (!opts.default && options.default) opts.default = options.default
+  else if (opts.default) options.default = opts.default
+  else if (!options.default && !opts.default)
+    throw new Error('Please pass a defaults file - it can be empty')
+
+  if (!opts.storage && options.storage) opts.storage = options.storage
+  else if (opts.storage) options.storage = opts.storage
+  else if (!opts.storage && !opts.storage)
+    throw new Error('Please pass a storage location')
+
+  if (!opts.reset && !opts.refresh) {
+    return new Promise((resolve, reject) => {
+      if (database) resolve(database)
+      else {
+        try {
+          fs.exists(opts.storage, (exists) => {
             if (exists) {
-              fs.copyFile(defaults, userPref, (err) => {
-                if (err) console.log(err)
-                fs.readFileSync(userPref, (data) => {
-                  database = JSON.parse(data)
-                  resolve(database)
-                })
+              try {
+                database = JSON.parse(fs.readFileSync(opts.storage))
+                resolve(database)
+              } catch {
+                throw new Error(
+                  "Couldn't read the file. Make sure it's JSON parsable."
+                )
+              }
+            } else {
+              fs.exists(opts.default, (exists) => {
+                if (exists) {
+                  fs.copyFile(opts.default, opts.storage, (err) => {
+                    if (err) reject(err)
+                    else {
+                      try {
+                        database = JSON.parse(fs.readFileSync(opts.storage))
+                        resolve(database)
+                      } catch {
+                        throw new Error(
+                          "Couldn't read the file. Make sure it's JSON parsable."
+                        )
+                      }
+                    }
+                  })
+                }
               })
             }
           })
+        } catch (err) {
+          reject(err)
         }
+      }
+    })
+  } else if (!opts.reset && opts.refresh) {
+    return new Promise((resolve, reject) => {
+      fs.exists(opts.storage, (exists) => {
+        if (exists) {
+          try {
+            database = JSON.parse(fs.readFileSync(opts.storage))
+            resolve(database)
+          } catch {
+            reject("Couldn't read the file. Make sure it's JSON parsable.")
+          }
+        } else reject("Couldn't find the preferences file!")
       })
-    } catch (err) {
-      reject(err)
+    })
+  } else if (opts.reset) {
+    return new Promise((resolve, reject) => {
+      fs.copyFile(opts.default, opts.storage, (err) => {
+        if (err) reject(err)
+        fs.readFileSync(opts.storage, (data) => {
+          database = JSON.parse(data)
+          resolve(database)
+        })
+      })
+    })
+  }
+}
+
+// builds the easy syntax passed by a request into a JSON object for merging with main JSON Storage blob
+const construct = (selection, value, callback) => {
+  let built = {}
+  let array = selection.split('.')
+  // .split returns length == 1 with 0 matches
+  if (array.length == 1) {
+    if (typeof value == 'object') {
+      built = JSON.parse('{"' + selection + '": {}}')
+      built[selection] = value
+    } else built = JSON.parse('{' + selection + ':' + value + '}')
+  } else {
+    try {
+      array.forEach((item, index) => {
+        let helper = {}
+        let passed = item.toString()
+        index == array.length - 1
+          ? (helper[passed] = value) // assign the last specified object the value passed
+          : (helper[passed] = {}) // assign an empty object for parents
+        Object.assign(built, helper)
+      })
+    } catch {
+      null
+      // last value of array is undefined,
+      // so 'Object' throws an error when trying to assign
+    }
+
+    for (i = array.length - 1; i > 0; i--) {
+      let helper
+      let key = Object.keys(built)[i]
+      let value = built[key]
+      typeof value == 'object'
+        ? (helper = JSON.parse('{"' + key + '":' + JSON.stringify(value) + '}')) // convert key with typeof JSON (assigned a value) to string for parsing back to JSON
+        : (helper = JSON.parse('{"' + key + '":' + value + '}')) // prevent ""_string_""
+      built[Object.keys(built)[i - 1]] = helper
+      delete built[Object.keys(built)[i]] // cleanup the bumped item
     }
   }
-})
-
-const get = (options) => {
-  return new Promise((resolve, reject) => {
-    initialise
-      .then((database) => {
-        let data
-        let key
-        if (typeof options == 'object' && Object.keys(options)[0]) {
-          if (options.keys) {
-            if (options.name) key = 'database.' + options.name.toString()
-            else key = 'database'
-            options.keys.forEach((entry) => {
-              key += '.' + entry
-            })
-            data = eval(key) // return sub-key (depth < 0) value
-            resolve(data)
-          } else {
-            data = database[options.name.toString()]
-            resolve(data)
-          } // return depth 0 key value
-        } else {
-          data = database
-          resolve(data)
-        } // return all key:value pairs
-      })
-      .catch((err) => reject(err))
-  }).catch((err) => reject(err))
+  callback(built)
 }
 
 // Set the key:value pair, or a sub key:value pair if requested
-const set = (options) => {
-  return new Promise((resolve, reject) => {
-    initialise
-      .then((database) => {
-        const getDepth = ({children}) =>
-          1 + (children ? Math.max(...children.map(getDepth)) : 0)
-        const depth = getDepth(options.value)
-        let output
-        depth
-          ? (output = _object.merge(database, options.value)) // merge recursively without overwriting
-          : () => {
-              database[options.name] = options.value // update single 0 depth key:value pair
-              output = database
-            }
-        fs.writeFile(userPref, JSON.stringify(output), (err) => {
-          if (err) reject(err)
-          resolve('The file was saved')
+const set = async (selection, value, callback, refresh) => {
+  if (selection && typeof selection == 'string' && typeof value !== undefined) {
+    await initialise({'refresh': refresh}).then((database) => {
+      construct(selection, value, (data) => {
+        let output = _object.merge(database, data) // <3 to lodash
+        fs.writeFile(options.storage, JSON.stringify(output), (err) => {
+          if (typeof callback == 'function') {
+            if (err) callback(false, err)
+            else callback(true)
+          } else {
+            if (err) reject(err)
+            else return true
+          }
         })
       })
-      .catch((err) => reject(err))
-  }).catch((err) => reject(err))
+    })
+  }
 }
 
-const reset = async (options) => {
-  return new Promise((resolve, reject) => {
-    initialise.then((database) => {
-      fs.readFile(userPref, (err, data) => {
-        if (err) console.log('here ' + err)
-        original = JSON.parse(data)
-
-        // depth
-        //   ? (output = _object.merge(database, options.value)) // merge recursively without overwriting
-        //   : () => {
-        //       database[options.name] = original[options.name] // update single 0 depth key:value pair
-        //       output = database
-        //     }
-        // resolve(database)
-      })
+const reset = async (selection, callback) => {
+  let value
+  if (
+    selection &&
+    typeof selection == 'string' &&
+    typeof callback == 'function'
+  ) {
+    fs.readFile(defaults, (err, data) => {
+      if (err) callback(false, err)
+      else {
+        let original = JSON.parse(data)
+        let key = 'original.' + selection
+        try {
+          value = eval(key)
+          set(selection, value, callback)
+        } catch {
+          callback(false, "Couldn't find that setting in defaults.")
+        }
+      }
     })
-  })
+  } else if (!selection && !callback) {
+    callback(false, 'Please pass parameters.')
+  } else if (
+    selection &&
+    typeof selection !== 'string' &&
+    typeof callback == 'function'
+  ) {
+    callback(
+      false,
+      'Expected selection to be a string, but got ' + typeof selection
+    )
+  } else return 'Your parameters were incorrect in an unexpected way.'
+}
+
+const get = async (selection, callback, refresh) => {
+  let data
+  await initialise({'refresh': refresh})
+    .then((database) => {
+      if (typeof selection !== 'function') {
+        let key = 'database.' + selection
+        try {
+          data = eval(key) // return one pair
+          return true
+        } catch {
+          return true // data = undefined;
+        }
+      } else {
+        data = database // return full json object
+        return true
+      }
+    })
+    .catch((err) => {
+      callback(false, err)
+    })
+  if (typeof callback == 'function') {
+    if (data) callback(data)
+    else callback(false, "Couldn't find that setting in storage.")
+  } else return data
 }
 
 module.exports = {
   initialise,
   set,
-  get,
-  reset
+  reset,
+  get
 }
